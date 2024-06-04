dabc02922109cd0edfc00c84965caf56d81053f3
afghanelyas <afghan.elyas1@gmail.com>
2024-06-04 23:03:22 +0430

    added the route

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,8 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\Authentication\RegisterController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [AuthController::class, 'login']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
 
 // Authentication routes
 Route::post('/register', [RegisterController::class, 'store'])
